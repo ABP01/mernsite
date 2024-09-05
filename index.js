35bb0ddad672af0e4d4da7787cf56b130f320e49
Armel Bogue <komlanbogue@gmail.com>
2024-09-05 10:06:32 +0000

    "Added nodemon dependency and updated start script to use nodemon with babel-node"

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const port = 4000;
 
+app.get("/", (_, res) => {
+    res.send("Hello Express!!!")
+})
+
 app.listen(port, () => {
     console.log(`Serveur démaré avec succès sur le port : ${port}`);
 }) 
