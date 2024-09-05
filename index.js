7768cfa96f182bc7df4f6d1c5ee4f54b982ea72a
Armel Bogue <komlanbogue@gmail.com>
2024-09-05 23:18:29 +0000

    "Updated index.js to import express and MongoDB, added MongoDB connection code, and updated package-lock.json and package.json to include MongoDB dependency"

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-const express = require("express");
+import express from "express";
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const app = express();
 const port = 4000;
 
+
+const uri = "mongodb+srv://komlanbogue:Jolieabp2005@mernsite.4j0q1.mongodb.net/?retryWrites=true&w=majority&appName=mernsite";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    await client.close();
+  }
+}
+
+run().catch(console.dir);
+
 app.get("/", (_, res) => {
-    res.send("Hello Express!!!")
+  res.send("Hello Express!!!")
 })
 
 app.listen(port, () => {
-    console.log(`Serveur démaré avec succès sur le port : ${port}`);
-}) 
+  console.log(`Serveur démaré avec succès sur le port : ${port}`);
+})
